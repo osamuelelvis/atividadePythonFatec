2f90a452ab17a5de3a5a0873c6bb18fd49ec37d5
osamuelelvis <elvislimasamuel27@gmail.com>
2026-02-27 11:35:10 -0300

    Adição de código

---
+++ Exercicio4.py
@@ -0,0 +1,6 @@
+# Exercício 4: Conversão de celsius para fahrenheit
+celsius = int;
+fahrenheit = int;
+celsius = int(input("Digite a temperatura em Celsius: "));
+fahrenheit = (celsius * 9/5) + 32;
+print("A temperatura em Fahrenheit é: ", fahrenheit);
