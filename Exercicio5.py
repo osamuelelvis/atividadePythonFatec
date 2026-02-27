1891c40c3114348aca463c81b68644d8ce073b2a
osamuelelvis <elvislimasamuel27@gmail.com>
2026-02-27 11:41:57 -0300

    Exercício 5: Equação de segundo grau

---
+++ Exercicio5.py
@@ -0,0 +1,9 @@
+# Exercicio 5: Equação de segundo grau
+a = int;
+b = int;
+c = int;
+a = int(input("Digite o valor de A: "));
+b = int(input("Digite o valor de B: "));
+c = int(input("Digite o valor de C: "));
+delta = (b**2) - (4 * a * c);
+print("O valor de delta é:", delta);
