92262b168afcb4a0dff6ee1e1c2f41c713f9da7b
EmbryND <elvislimasamuel27@gmail.com>
2026-02-28 12:14:03 -0300

    Exercício 17: Cálculo do consumo de combustível

---
+++ Exercicio17.py
@@ -0,0 +1,5 @@
+# Exercício 17: Cálculo do consumo de combustível
+tempo_percurso = float(input("Digite o tempo gasto no percurso (em horas): "))
+velocidade_media = float(input("Digite a velocidade média (em km/h): "))
+quantidade_gasta_litros = (velocidade_media * tempo_percurso) / 12  # Consumo de combustível: 1 litro a cada 12 km
+print("A quantidade de combustível gasta no percurso é:", quantidade_gasta_litros, "litros")
