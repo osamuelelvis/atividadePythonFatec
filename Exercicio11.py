65958427903c0c04a0b0352c28215fb76a45397e
EmbryND <elvislimasamuel27@gmail.com>
2026-02-28 11:21:44 -0300

    Exercício 11: Comprimento do círculo

---
+++ Exercicio11.py
@@ -0,0 +1,5 @@
+# Exercício 11: Comprimento do círculo
+import math
+raio_circunferencia = float(input("Digite o raio da circunferência: "))
+comprimento = 2 * math.pi * raio_circunferencia
+print("O valor da circunferência do círculo é:", comprimento)
