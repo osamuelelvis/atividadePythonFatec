6662a582f6b192e6e9aab8b032e402499cd1b39b
EmbryND <elvislimasamuel27@gmail.com>
2026-02-28 11:47:48 -0300

    Exercício 14: Terceiro ângulo do triângulo

---
+++ Exercicio14.py
@@ -0,0 +1,5 @@
+# Exercício 14: Terceiro ângulo do triângulo
+primeiro_angulo = int(input("Digite o valor do primeiro ângulo do triângulo: "))
+segundo_angulo = int(input("Digite o valor do segundo ângulo do triângulo: "))
+terceiro_angulo = 180 - (primeiro_angulo + segundo_angulo)
+print("O terceiro ângulo do triângulo é:", terceiro_angulo)
