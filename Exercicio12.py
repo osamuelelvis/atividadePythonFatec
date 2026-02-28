2877f89903559455084a88c8cb2e57b6a644a4bf
EmbryND <elvislimasamuel27@gmail.com>
2026-02-28 11:26:30 -0300

    Exercício 12: Idade daqui a 17 anos

---
+++ Exercicio12.py
@@ -0,0 +1,6 @@
+# Exercicio 12: Idade daqui a 17 anos
+ano_atual = 2026
+ano_nascimento = int(input("Digite o seu ano de nascimento: "))
+idade = ano_atual - ano_nascimento
+idade_futuro = idade + 17
+print("Você terá daqui a 17 anos", idade_futuro,"anos")
