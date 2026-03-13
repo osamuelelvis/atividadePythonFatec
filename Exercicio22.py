dd4c581271761f0b74e431e9bf4e33df6a65e682
EmbryND <elvislimasamuel27@gmail.com>
2026-03-12 23:12:45 -0300

    Exercício 22: Valores em ordem crescente

---
+++ Exercicio22.py
@@ -0,0 +1,10 @@
+# Exercício 22: Valores em ordem crescente
+# Declarar
+n1: int = int(input("Digite o primeiro número: "))
+n2: int = int(input("Digite o segundo número: "))
+# Início
+if n1 > n2:
+    print("Os números em ordem crescente são:", n1, "e", n2)
+elif n2 > n1:
+    print("Os números em ordem crescente são:", n2, "e", n1)
+# Fim
