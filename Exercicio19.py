b83853d61c4a2fc64370db5ddd62dfd126b466e4
EmbryND <elvislimasamuel27@gmail.com>
2026-03-12 22:48:03 -0300

    Exercício 19: Comparar maior e menor

---
+++ Exercicio19.py
@@ -0,0 +1,10 @@
+# Exercício 19: Comparar maior e menor
+# Declarar
+n1: int = int(input("Digite o primeiro número: "))  
+n2: int = int(input("Digite o segundo número: "))
+# Inicio
+if n1 > n2:
+    print("O maior número é:", n1)
+elif n2 > n1:
+    print("O maior número é", n2)
+# Fim
