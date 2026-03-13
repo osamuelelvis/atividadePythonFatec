bd99bdf58177da014685d904e56e83e6b1facf10
EmbryND <elvislimasamuel27@gmail.com>
2026-03-12 23:22:16 -0300

    Exercício 23: Valores em ordem crescente - pt.2

---
+++ Exercicio23.py
@@ -0,0 +1,11 @@
+# Exercício 23: Valores em ordem crescente - pt.2
+# Declarar
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+n3 = int(input("Digite o terceiro número: "))
+n4 = int(input("Digite o quarto e último número: "))
+# Início
+numeros = [n1, n2, n3, n4]
+numeros.sort()
+print("Os números em ordem crescente são:", numeros)
+# Fim
