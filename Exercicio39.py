5a29e6bc57f5c8eb98e745e65c4ac8e0999ffd28
osamuelelvis <elvislimasamuel27@gmail.com>
2026-03-20 14:17:32 -0300

    Exercício 39:	Calcule a quantidade de grãos contidos em um tabuleiro de xadrez. Pequena correção.

--- Exercicio39.py
+++ Exercicio39.py
@@ -1,1 +1,14 @@
-# Exercício 39: 39.	Calcule a quantidade de grãos contidos em um tabuleiro de xadrez
+# Exercício 39:	Calcule a quantidade de grãos contidos em um tabuleiro de xadrez
+# Declarar
+casa: int
+quantidade: int = 1
+total: int = 0
+# Início
+for casa in range(1, 65):
+	total += quantidade
+	if casa < 64:
+		quantidade *= 2
+
+print(f"Quantidade na casa 64: {quantidade}")
+print(f"Total de grãos no tabuleiro: {total}")
+# Fim
