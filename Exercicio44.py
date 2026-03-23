c6e209c8de69a9b3f4d412e867e6164baa1b01bb
osamuelelvis <elvislimasamuel27@gmail.com>
2026-03-23 11:18:28 -0300

    Exercício 44: Cálculo do valor da potência

---
+++ Exercicio44.py
@@ -0,0 +1,10 @@
+# Exercício 44: Cálculo do valor da potência
+# Declarar
+base: int
+expoente: int
+# Início
+base = int(input("Digite a base: "))
+expoente = int(input("Digite o expoente: "))
+resultado = base ** expoente
+print("O resultado da potência é:", resultado)
+# Fim
